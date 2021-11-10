cf8b200370bb88e044f33fd87021e3c89e132514
lsj94 <lsj1994@kinx.net>
2021-11-10 14:05:08 +0900

    circle 그래프

---
+++ 초등학생/2.2668.py
@@ -0,0 +1,24 @@
+def dfs(v,i):
+    visited[v] = 1
+    for s in load[v]:
+        if not visited[s]:
+            dfs(s,i)
+        elif visited[s] and s ==i:
+            res.append(s)
+
+
+n = int(input())
+load = [[] for _ in range(n+1)]
+
+for i in range(1,n+1):
+    num = int(input())
+    load[i].append(num)
+length = 0
+res = []
+for i in range(1,n+1):
+    visited = [0] * (n+1)
+    dfs(i,i)
+
+print(len(res))
+for r in res:
+    print(r)
