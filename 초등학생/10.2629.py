9084a13205ce1b7f3b6fd9f5f300b864044ef2aa
lsj94 <lsj1994@kinx.net>
2021-12-08 15:29:47 +0900

    dp 백트래킹

--- 초등학생/10.2629.py
+++ 초등학생/10.2629.py
@@ -1,21 +1,28 @@
+def calcu(cur, cur_weight):
+    tmp = dp[cur][cur_weight]
+    if (tmp != 0) : return
+    
+    dp[cur][cur_weight] = 1 
+    if cur >= n: return
+    
+    calcu(cur+1, cur_weight + chu[cur])
+    calcu(cur+1, cur_weight)
+    calcu(cur+1, abs(cur_weight - chu[cur]))
+
 n = int(input()) # 추 갯수
 chu = list(map(int,input().split()))
 num = int(input()) # 구슬 갯수 
 gu = list(map(int,input().split()))
+# 고려 사항 
+# 1. 구슬이 없는 저울에 추를 추가  | 2. 추가하지 않는 경우  |  3. 구슬이 있는 저울에 추를 추가 
+max_weigh = sum(chu)
+dp = [[0 for _ in range(500*300+1)] for _ in range(31)] 
 
-s,e = 0, 0
-p = set()
-while s != n and e !=n:
-    for i in range(e,n):
-        tmp = sum(chu[s:i+1])
-        print(tmp)
-        p.add(tmp)
-    s+= 1
-    e+= 1
-p = list(p)
-# print(p)
+
+calcu(0,0)
+
+last_arr = dp[n]
 for g in gu:
-    if g in p:
-        print('Y')
-    else:
-        print('N')
+    if last_arr[g] == 1:
+        print('Y', end=" ")
+    else: print('N', end=" ")
